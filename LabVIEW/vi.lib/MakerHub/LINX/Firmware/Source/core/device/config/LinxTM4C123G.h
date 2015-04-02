64615bf7fd27f3d59a576d16a6936ec087c77a5d
Sam K <labviewhacker@gmail.com>
2015-04-02 12:33:30 -0500

    Adding TM4C123G Firmware (not yet working)

---
+++ LabVIEW/vi.lib/MakerHub/LINX/Firmware/Source/core/device/config/LinxTM4C123G.h
@@ -0,0 +1,25 @@
+/****************************************************************************************
+**  LINX Configuration data for the TI Tivia TM4C123G
+**
+**  For more information see:           www.labviewmakerhub.com/linx
+**  For support visit the forums at:    www.labviewmakerhub.com/forums/linx
+**  
+**  Written By Sam Kristoff
+**
+** BSD2 License.
+****************************************************************************************/		
+
+#ifndef LINXCONFIG
+
+	#define LINXCONFIG
+
+	#define DEBUG_ENABLED 0
+	#define NUM_I2C_CHANS 1
+	#define NUM_SPI_CHANS 1
+	#define NUM_UART_CHANS 1
+	#define DEVICE_FAMILY 6
+
+	#define ARDUINO_VERSION 160
+		
+#endif //LINXCONFIG
+
